b4d4b028c59997c3191a51d244b032bad4f6b268
Bruno <84346708+BrunoRawan@users.noreply.github.com>
2021-12-01 08:56:47 -0300

    Delete thalita.py

--- thalita.py
+++
@@ -1,9 +0,0 @@
-nome = str(input('Digite seu nome: '))
-usuario = 'piquitin', 'thalita'
-if nome == 'piquitin':
-    print('Oi piquitao S2.')
-elif nome == 'thalita':
-    print('Oi piquitao S2.')
-else:
-    print('Você não é meu amor....')
-input()
