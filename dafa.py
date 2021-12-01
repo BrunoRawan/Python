9b393921b409c1d0dcbced2525f0e3a444f60afe
Bruno <84346708+BrunoRawan@users.noreply.github.com>
2021-12-01 08:57:54 -0300

    Delete dafa.py

--- dafa.py
+++
@@ -1,8 +0,0 @@
-import pyautogui
-https://www.youtube.com/watchv=QisvOa8casU
-
-pyautogui.hotkey('ctrl', 't')
-pyautogui.write('https://www.youtube.com/watch?v=QisvOa8casU')
-pyautogui.press('enter')
-
-
